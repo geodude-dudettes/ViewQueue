200154343ca636ae3f3fd34a4a15dee7dd0b7cb2
Rachel Dean <rchldn@gmail.com>
2025-03-11 12:30:44 -0400

    MUI rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import React from 'react';
+// import * as React from 'react'; namespace import, if using typescript and/or hooks
+import Button from '@mui/material/Button';
+
 function App() {
-return (
-    <h1>ViewQueue</h1>
-)
+  return (
+    <>
+      <h1>ViewQueue</h1>
+
+      <Button variant='outlined'>Hello World</Button>
+    </>
+  );
 }
 
-
-
-
-export default App
+export default App;
