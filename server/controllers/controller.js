be11933da18ea27af5b66c72ce5f0e2ae4978fc5
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-03-11 12:32:14 -0400

    feat: setting up controller and middleware design

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,7 +2,18 @@ import db from '../models/model';
 
 const Controller = {};
 
-Controller.allMedia = (req, res, next) => {};
+Controller.allMedia = (req, res, next) => {
+    db.query('SELECT * FROM media')
+    .then((result) => {
+        res.locals.media = result
+        return next()
+    })
+    .catch((err) => next(err));
+};
+
+// watch list (to be discussed)
+// favorite list
+// media list
 
 Controller.createMedia = (req, res, next) => {};
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,17 +4,17 @@ import controller from '../controllers/controller';
 const router = express.Router();
 
 router.get('/', controller.allMedia, (req, res) => {
-  return res.status(200).send();
+  return res.status(200).send(`Successfully retireved all media queries!`);
 });
 
 router.post('/', controller.createMedia, (req, res) => {
-  return res.status(200).send();
+  return res.status(200).send(`Successfully added query: ${res.locals.postMedia}`);
 });
 
 router.patch('/:id', controller.editMedia, (req, res) => {
-  return res.status(200).send();
+  return res.status(200).send(`Successfully updated query: ${res.locals.postMedia}`);
 });
 
 router.delete('/:id', controller.deleteMedia, (req, res) => {
-  return res.status(200).json();
+  return res.status(200).json(`Successfully deleted query: ${res.locals.postMedia}`);
 });
