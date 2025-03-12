82afe5f201af23f5bed3ee0e182c43e3e69c625d
Rachel Dean <rchldn@gmail.com>
2025-03-12 17:19:13 -0400

    scooped up my other changes too, forgot to stage

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,34 @@ import Button from '@mui/material/Button';
 import { useEffect, useState } from 'react';
 
 const databaseURL = 'http://localhost:5173/';
+const searchInput = document.getElementById('title');
 
-// const addToWatchList = () => {};
+function addToWatchList () {};
+
+function handleSubmitClick() {
+    const title = searchInput.value;
+
+    if (title) {
+    fetch(databaseURL)
+    .then((response) => {
+        if (response.ok)
+            return response.json();
+        else throw new Error('response contained an error');
+        // may need to add error handling if someone misspelled something (aka if the database doesn't return anything)
+    })
+    .then((selectedMovie) => {
+        renderMovie(selectedMovie);
+        // the above would mean that now ONLY the movie they searched for is showing up on the UI
+        searchInput.value = '';
+        //emptying out the search value in the input textbox
+    });
+} else {
+    alert('Please enter a title');
+}
+};
 
 //render each movie using React
-function renderMovie(movie, { addToWatchList }) {
+function renderMovie(movie) {
   const title = movie.title;
   const type = movie.type;
   const genre = movie.genre;
@@ -21,10 +44,11 @@ function renderMovie(movie, { addToWatchList }) {
 
 
 
+
   return (
     <div>
-      <img src={imgURL} onMouseOver={e => console.log(e, 'image description here, fill this part out later')} />
-      {/* for the above - source would be the destructured image URL off the db response body right? */}
+      <img src={imgURL} onMouseOver={e => console.log(e, 'movie description here, fill this part out later')} />
+      {/* for the above, i didn't get to the point where we are able to see the movie description when you hover over the image, but this is the basic idea */}
       <p>{title}</p>
       <p>{type}</p>
       <p>{genre}</p>
@@ -35,8 +59,6 @@ function renderMovie(movie, { addToWatchList }) {
     </div>
   );
 }
-
-// !  need to reacte an addToWatchList function, but would that live here or in the watchList page?
 
 function App() {
   //fetch request to get the database entries, once per page load
@@ -57,10 +79,17 @@ function App() {
   return (
     <>
       <h1>ViewQueue</h1>
-      {/* add search */}
+      <div>
+        <label>Search for a Title:<input type="text" id="title" name="title" /></label>
+        <Button variant='contained' id="searchButton" onClick={handleSubmitClick}>
+        Search
+      </Button>
+      </div>
 
       <Button variant='contained'>Hello World</Button>
-      <>renderMovie();</>
+
+      <>renderMovie</>
+      {/* the above is wrong - but how do i get the contents of the renderMovie function into a div here? */}
     </>
   );
 }
