fb72ddfc47daef02fe983145c2279dcfdbcf7be3
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-03-12 12:17:25 -0400

    fix: modified methods appropriate for MVP

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,11 +2,12 @@ import db from '../models/model';
 
 const Controller = {};
 
+// We'll have to hand populate the data using this method
 Controller.allMedia = (req, res, next) => {
-    db.query('SELECT * FROM media')
+  db.query('SELECT * FROM media;')
     .then((result) => {
-        res.locals.media = result
-        return next()
+      res.locals.media = result.rows;
+      return next();
     })
     .catch((err) => next(err));
 };
@@ -15,10 +16,43 @@ Controller.allMedia = (req, res, next) => {
 // favorite list
 // media list
 
-Controller.createMedia = (req, res, next) => {};
+// // just in case the user wants to create data in the future :)
+// Controller.createMedia = (req, res, next) => {
+//     const { id, title, type, genre, year, description, img_url, favorite, watch_list } = req.body;
 
-Controller.editMedia = (req, res, next) => {};
+//     const query = `
+//         INSERT INTO media (id, title, type, genre, year, description, img_url, favorite, watch_list)
+//         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9 )
+//         RETURNING *;
+//         `;
 
+//     db.query(query, [id, title, type, genre, year, description, img_url, favorite, watch_list])
+//     .then((result) => {
+//         res.locals.createdMedia = result.row[0]
+//         return next()
+//     })
+//     .catch((err) => next(err));
+// };
+
+// make single middleware for watch list
+// favorite is separate
+
+Controller.editMedia = (req, res, next) => {
+  const { id } = req.params;
+
+  const query = `UPDATE media SET watch_list = TRUE WHERE id = $1
+RETURNING *;`;
+
+  db.query(query, [id])
+    .then((result) => {
+      res.locals.updatedMedia = result.rows[0];
+      return next();
+    })
+    .catch((err) => next(err));
+};
+
+// deleting from watch list
+// just change it to false
 Controller.deleteMedia = (req, res, next) => {};
 
 export default Controller;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -7,9 +7,9 @@ router.get('/', controller.allMedia, (req, res) => {
   return res.status(200).send(`Successfully retireved all media queries!`);
 });
 
-router.post('/', controller.createMedia, (req, res) => {
-  return res.status(200).send(`Successfully added query: ${res.locals.postMedia}`);
-});
+// router.post('/', controller.createMedia, (req, res) => {
+//   return res.status(200).send(`Successfully added query: ${res.locals.postMedia}`);
+// });
 
 router.patch('/:id', controller.editMedia, (req, res) => {
   return res.status(200).send(`Successfully updated query: ${res.locals.postMedia}`);
