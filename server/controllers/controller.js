f0a43a8ffaaa6b465dae4a6117c10412b3863b49
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-03-13 10:22:28 -0400

    feat: - refactored crud operations - added error helper function - added pseudocode

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,22 +1,38 @@
 import db from '../models/model';
 
+/*
+ * An error helper function to ensure consistent formatting of error logs
+ * before it passes to the global error handler
+ */
+const createError = ({ log, status = 500, message }) => {
+  return { log, status, message };
+};
+
+/* This is the object that contains all of the methods */
 const Controller = {};
 
-// We'll have to hand populate the data using this method
+/** retrieve all the media */
 Controller.allMedia = (req, res, next) => {
   db.query('SELECT * FROM media;')
     .then((result) => {
       res.locals.media = result.rows;
       return next();
     })
-    .catch((err) => next(err));
+    .catch((err) => {
+      /* The 'error' variable stores the evaluated result of the createError helper function */
+      const error = createError({
+        log: `Error occurred: ${err}`,
+        status: 500,
+        message: { err: 'Failure to retrieve all media from database' },
+      });
+      return next(error); // passes the formatted error to the global error handler
+    });
 };
 
-// watch list (to be discussed)
-// favorite list
-// media list
-
-// // just in case the user wants to create data in the future :)
+/*
+ * Just in case the user wants to create data in the future :)
+ * End users can hand populate the data using this method.
+ */
 // Controller.createMedia = (req, res, next) => {
 //     const { id, title, type, genre, year, description, img_url, favorite, watch_list } = req.body;
 
@@ -34,10 +50,12 @@ Controller.allMedia = (req, res, next) => {
 //     .catch((err) => next(err));
 // };
 
-// make single middleware for watch list
-// favorite is separate
+/*
+ * This is the middleware design solely for adding or removing media from watch list
+ */
 
-Controller.editMedia = (req, res, next) => {
+/* adding media to watch list using UPDATE operation */
+Controller.toWatch = (req, res, next) => {
   const { id } = req.params;
 
   const query = `UPDATE media SET watch_list = TRUE WHERE id = $1
@@ -45,14 +63,39 @@ RETURNING *;`;
 
   db.query(query, [id])
     .then((result) => {
-      res.locals.updatedMedia = result.rows[0];
+      res.locals.addToWatch = result.rows[0];
       return next();
     })
-    .catch((err) => next(err));
+    .catch((err) => {
+      const error = createError({
+        log: `Error occurred: ${err}`,
+        status: 500,
+        message: { err: 'Failed to ADD media query to watch list' },
+      });
+      return next(error);
+    });
 };
 
-// deleting from watch list
-// just change it to false
-Controller.deleteMedia = (req, res, next) => {};
+/* Remove media from watch list using UPDATE operation once more */
+Controller.notWatch = (req, res, next) => {
+  const { id } = req.params;
+
+  const query = `UPDATE media SET watch_list = FALSE WHERE id = $1
+  RETURNING *;`;
+
+  db.query(query, [id])
+    .then((result) => {
+      res.locals.removeWatch = result.rows[0];
+      return next();
+    })
+    .catch((err) => {
+      const error = createError({
+        log: `Error occurred: ${err}`,
+        status: 500,
+        message: { err: 'Failed to REMOVE media query from watch list' },
+      });
+      return next(error);
+    });
+};
 
 export default Controller;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,20 +1,31 @@
 import express from 'express';
 import controller from '../controllers/controller';
 
+/**
+ * Importing the Express Framework:
+ * Create a new router instance using Express
+ * This `router` object will be used to define and organize routes
+ * It helps modularize route handling instead of defining everything in `server.js`
+ * Requests matching these routes will be directed to the appropriate controller functions
+ */
 const router = express.Router();
 
 router.get('/', controller.allMedia, (req, res) => {
-  return res.status(200).send(`Successfully retireved all media queries!`);
+  return res.status(200).json(res.locals.media);
 });
 
+/* No need for end user to create data on their own */
 // router.post('/', controller.createMedia, (req, res) => {
 //   return res.status(200).send(`Successfully added query: ${res.locals.postMedia}`);
 // });
 
-router.patch('/:id', controller.editMedia, (req, res) => {
-  return res.status(200).send(`Successfully updated query: ${res.locals.postMedia}`);
+/* Patch Requests for adding to and removing from Watch List */
+router.patch('/:id', controller.toWatch, (req, res) => {
+  return res.status(200).json(res.locals.addToWatch);
 });
 
-router.delete('/:id', controller.deleteMedia, (req, res) => {
-  return res.status(200).json(`Successfully deleted query: ${res.locals.postMedia}`);
+router.patch('/:id', controller.notWatch, (req, res) => {
+  return res.status(200).json(res.locals.removeWatch);
 });
+
+// stretch feature: adding a delete operation
